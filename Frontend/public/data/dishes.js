78400846bcf23390f567922c1a8df27a8384d918
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-05-22 22:51:59 +0530

    more progress

--- Frontend/public/data/dishes.js
+++ Frontend/public/data/dishes.js
@@ -1,6 +1,8 @@
 // data/dishes.js
+import '../assets/images/chicken_kebab.jpg'
+
 export const dishes = [
-    { id: 1, name: "Margherita Pizza", price: 10, desc: "Classic cheese pizza", img: "🍕" },
+    { id: 1, name: "Margherita Pizza", price: 10, desc: "Classic cheese pizza", img: "../assets/images/chicken_kebab.jpg" },
     // { id: 2, name: "Caesar Salad", price: 7, desc: "Fresh lettuce with dressing", img: "🥗" },
     // { id: 3, name: "Pasta Carbonara", price: 12, desc: "Creamy Italian pasta", img: "🍝" }
 ];
--- Frontend/src/Components/MenuCard/MenuCard.jsx
+++ Frontend/src/Components/MenuCard/MenuCard.jsx
@@ -1,16 +1,32 @@
-
-
 //  { id: 1, name: "Margherita Pizza", price: 10, desc: "Classic cheese pizza", img: "🍕" },
-
+import './MenuCard.css'
 function MenuCard({key, name, desc, price, image}) {
+
+  /*
+  border-2 
+  bg-white 
+  text-black
+  aspect-[3/4]
+  w-sm
+  rounded-md
+  */
+
   return (
     <>
-        <div className="border-2">
-            ldvknslvkn
-            <img src={image} alt="Dish Image" />
-            <h3>{name}</h3>
-            <p>{desc}</p>
-            <p>{price}</p>
+        <div className="container">
+          <img 
+          className="card-img"
+          src={image} alt="Dish Image" 
+          />
+
+          <h3
+          className="card-heading"
+          >{name}</h3>
+          <p>{desc}</p>
+          <p>{price}</p>
+
+          <button>Add to Cart</button>
+          <button>Save</button>
         </div>
     </>
   )
