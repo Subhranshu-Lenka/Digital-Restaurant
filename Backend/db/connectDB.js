bde6b6856f0deb7a03856a69cffd434e0c0ac855
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-06-01 16:57:48 +0530

    connection to database and app setup in backend

---
+++ Backend/db/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URI);
+        if (connectionInstance) {
+            console.log("Connected to Mongo Database Successfully.");
+        }
+        else {
+            throw Error("Mongo Authentication Fail Error.")
+        }
+    } catch (error) {
+        console.log("DB Connection Error ", error);
+        return
+    }
+}
+
+export default connectDB;
--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import connectDB from './db/connectDB.js';
+import express from 'express';
+
+const app = express();
+
+const Port = process.env.PORT || 8080;
+connectDB()
+.then(()=>{
+    app.listen(Port, ()=>{
+        console.log("Express App is listening on Port ",Port);
+    });
+})
+.catch((err)=> console.log("Connection Error: ",err));
