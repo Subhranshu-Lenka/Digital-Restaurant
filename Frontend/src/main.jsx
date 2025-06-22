04dc582f6c39833ca9dc5c6eefdc22f0492516f1
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2025-06-22 09:20:22 +0530

    fixed the env setup.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,10 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {GoogleOAuthProvider} from '@react-oauth/google';
 
-
-// const CLIENT_ID = "792659374495-phli3tvhq3icgi5rvoeq21nq4k34pqgm.apps.googleusercontent.com"
-const CLIENT_ID = "792659374495-phli3tvhq3icgi5rvoeq21nq4k34pqgm.apps.googleusercontent.com"
-//                 792659374495-phli3tvhq3icgi5rvoeq21nq4k34pqgm.apps.googleusercontent.com
+const CLIENT_ID =import.meta.env.CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
